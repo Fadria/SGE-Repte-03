be1cb21059095f4590db8d7a7a08270715b154e8
Federico Adrià Carrasco <fadriacarrasco@gmail.com>
2022-02-10 20:05:31 +0100

    Delete MVVM directory

--- MVVM/app/src/main/java/com/jovian/mvvm/model/QuoteModel.kt
+++
@@ -1,9 +0,0 @@
-package com.jovian.mvvm.model
-
-import com.google.gson.annotations.SerializedName
-
-//clase que almacena citas y su autor. Lo vamos a utilizar para leer los datos del retrofit
-data class QuoteModel(
-    @SerializedName("quote") val quote:String,
-    @SerializedName ("author") val author:String
-)
